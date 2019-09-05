c5be77585f7bea9d32c517ba1152d41ab5592387
Lisa Jo <lisaxjo@gmail.com>
2019-09-05 17:54:49 -0400

    finished the second tier

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,7 +2,34 @@
 
 // Complete this algo
 const minJumps = arr => {
+let counter = 0;
+let sum = 0
+let totalLength = arr.length
+// let array = arr.slice(sum,arr[sum])
+// let jumpSize = array.reduce(function(a, b){
+//   return Math.max(a, b)
+// })
+//console.log('jump size', jumpSize ,array)
 
+
+while(sum < totalLength){
+//   console.log(Math.max(...arr.slice(sum,arr[sum])))
+// let jumpSize = Math.max(...arr.slice(sum,arr[sum]))
+let array = arr.slice(sum,arr[sum])
+if(sum != totalLength -1){
+
+  let jumpSize = array.reduce(function(a, b){
+    return Math.max(a, b)})
+  console.log('jump size', jumpSize)
+  sum += jumpSize
+  console.log(sum)
+  counter++
+}
+else{
+  counter ++
+}
+ }
+ return counter
 };
 
 module.exports = minJumps
